5f3300b72c336bf15da1e1e44b2a236248396940
Douglas Angeli <douglasangeli92@gmail.com>
2021-01-30 15:02:17 -0300

    aula 2: state e linter

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
-import styled from "styled-components";
-import db from "../db.json";
-import Widget from "../src/components/Widget";
-import QuizBackground from "../src/components/QuizBackground";
-import Footer from "../src/components/Footer";
-import GitHubCorner from "../src/components/GitHubCorner";
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -17,8 +21,14 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>AluraQuiz - Douglas Angeli</title>
+      </Head>
       <QuizContainer>
         <Widget>
           <Widget.Header>
@@ -26,6 +36,20 @@ export default function Home() {
           </Widget.Header>
           <Widget.Content>
             <p>{db.description}</p>
+            <form
+              onSubmit={(infosDoEvento) => {
+                infosDoEvento.preventDefault();
+                console.log('Fazendo uma submissão por meio do React', infosDoEvento);
+                router.push(`quiz?name=${name}`);
+              }}
+            >
+              <input 
+                onChange={(infosDoEvento) => setName(infosDoEvento.target.value)}
+                placeholder="Diz aí seu nome" />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
         <Widget>
@@ -36,10 +60,9 @@ export default function Home() {
             <p>{db.description}</p>
           </Widget.Content>
         </Widget>
-
-        <Footer></Footer>
+        <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/douglasangeli/aluraquiz-douglasangeli"></GitHubCorner>
+      <GitHubCorner projectUrl='https://github.com/douglasangeli/aluraquiz-douglasangeli' />
     </QuizBackground>
   );
 }
---
+++ pages/quiz.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+
+export default function QuizPage() {
+  const router = useRouter();
+  return <h1>Página de Quiz</h1>;
+}
